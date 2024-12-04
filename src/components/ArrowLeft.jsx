349dc3621275dabfdc61e8fa1eab7c8c2ac7d068
nhenan <henan.noel@ynov.com>
2024-12-04 13:02:28 +0100

    Removed components that are useless

--- src/components/ArrowLeft.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ArrowLeft = () => {
-  return (
-    <div className="arrow-left"></div>
-  )
-}
-
-export default ArrowLeft;
--- src/components/ArrowRight.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ArrowRight = () => {
-  return (
-    <div className="arrow-right"></div>
-  )
-}
-
-export default ArrowRight;
