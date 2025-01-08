fe9a94b8578b8e3bdba2908d1f9224100efbcb83
ismael-dmn <ismael.dahmane@icloud.com>
2025-01-08 09:08:11 +0100

    Page About responsive

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,11 +4,16 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />
   }
 ])
 
