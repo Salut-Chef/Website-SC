66e6197d3f6a8f35e63020e8b067351ce7653d85
ismael-dmn <ismael.dahmane@icloud.com>
2025-01-09 14:27:19 +0100

    Page auth refaite

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import { auth } from "../config/firebase.js";
+import { auth } from "../config/firebase";
 import Header from "../layouts/Header";
 
 const Profile = () => {
