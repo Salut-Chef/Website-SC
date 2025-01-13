b22cf85d3574d882749c8e8b553b96118279fcf9
nhenan <henan.noel@ynov.com>
2025-01-13 10:59:18 +0100

    Just to checkout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
-import "../style/index.css";
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
 import { Link } from "react-router-dom";
